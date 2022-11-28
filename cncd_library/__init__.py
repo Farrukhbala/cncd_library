d5bf27cfe33b7c62ed6e2d2acee10cac6064d95d
Farrukh khan <35742633+Farrukhbala@users.noreply.github.com>
2022-11-28 15:13:21 +0500

    Update __init__.py

--- cncd_library/__init__.py
+++ cncd_library/__init__.py
@@ -1,6 +1,6 @@
 # This is so that you can import ppack or import average from ppack
 # in stead of from ppack.functions import average
 
-from .functions import CNCD
+# from .functions import CNCD
 
 
