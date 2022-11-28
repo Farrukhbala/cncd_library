849989a7c406dbf3118b61b4a15f2cc03b76fad8
Farrukh khan <35742633+Farrukhbala@users.noreply.github.com>
2022-11-28 15:09:45 +0500

    Update functions.py

--- cncd_library/functions.py
+++ cncd_library/functions.py
@@ -1,28 +1,24 @@
-class CNCD:
-    def __init__(self):
-        return None
+def checkPhenoCode(dataframe,phenotype): 
+    """ 
 
-    def checkPhenoCode(self,dataframe,phenotype): 
-        """ 
-        
-        This function checks disprencies in dataframes using phenotype files
+    This function checks disprencies in dataframes using phenotype files
 
-        Parameters:
-        dataframe (pandas dataframe): dataframe to be checked
+    Parameters:
+    dataframe (pandas dataframe): dataframe to be checked
 
-        phenotype (pandas dataframe): phenotype file
+    phenotype (pandas dataframe): phenotype file
 
-        
-        """ 
-        phenotype['status'] = 0
-        phenotype.loc[phenotype["disease_status"].str.contains("CASE"), "status"] = 1
-        phenotype.loc[phenotype["disease_status"].str.contains("CONTROL"), "status"] = 0
-        phenotype.loc[phenotype["disease_status"].str.contains("CASE/CONTROL"), "status"] = np.nan
-        phenotype.dropna(how='all',subset=['status'],inplace=True)
-        
-        incorrect_phenotypes = (dataframe.merge(phenotype, on=['center_alpha','status'], how='left', indicator=True)
-            .query('_merge == "left_only"')
-            .drop('_merge', 1))
-        incorrect_phenotypes = incorrect_phenotypes[['study_id','status','center_alpha']].copy()
-        df = pd.merge(incorrect_phenotypes,phenotype,how='inner',on=['center_alpha'],suffixes=('', '_correct'))
-        return(df[['study_id','status','center_alpha','disease_status','status_correct']])
+
+    """ 
+    phenotype['status'] = 0
+    phenotype.loc[phenotype["disease_status"].str.contains("CASE"), "status"] = 1
+    phenotype.loc[phenotype["disease_status"].str.contains("CONTROL"), "status"] = 0
+    phenotype.loc[phenotype["disease_status"].str.contains("CASE/CONTROL"), "status"] = np.nan
+    phenotype.dropna(how='all',subset=['status'],inplace=True)
+
+    incorrect_phenotypes = (dataframe.merge(phenotype, on=['center_alpha','status'], how='left', indicator=True)
+        .query('_merge == "left_only"')
+        .drop('_merge', 1))
+    incorrect_phenotypes = incorrect_phenotypes[['study_id','status','center_alpha']].copy()
+    df = pd.merge(incorrect_phenotypes,phenotype,how='inner',on=['center_alpha'],suffixes=('', '_correct'))
+    return(df[['study_id','status','center_alpha','disease_status','status_correct']])
