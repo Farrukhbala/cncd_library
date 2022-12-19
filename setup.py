7731b971815f4fd1b93c458c16e00ba175dcd128
Farrukh khan <35742633+Farrukhbala@users.noreply.github.com>
2022-12-19 10:48:29 +0500

    updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='cncd_library',
-    version='0.0.2',
+    version='0.0.3',
     author='Farrukh Khan',
     author_email='farrukh_bala@hotmail.com',
     description='Testing installation of Package',
